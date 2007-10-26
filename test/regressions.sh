522ab517d69fa020e68f90aaf1fa65a73659c989
milianw <milianw@c83574fe-1c5b-4f04-9049-3514c27e20bb>
2007-10-26 20:52:30 +0000

    added suite 4 to regression check
    git-svn-id: https://markdownify.svn.sourceforge.net/svnroot/markdownify/trunk@88 c83574fe-1c5b-4f04-9049-3514c27e20bb

--- test/regressions.sh
+++ test/regressions.sh
@@ -2,3 +2,4 @@
 php test.php --regressions --indented $@
 php test.php --regressions --indented --suite 2 $@
 php test.php --regressions --indented --suite 3 $@
+php test.php --regressions --indented --suite 4 $@
